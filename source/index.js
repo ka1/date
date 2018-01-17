d8a092666eb1c914d85d0648026f5f3cf47b08d2
Kai Kasugai <kk@formitas.de>
2018-01-17 17:44:48 +0100

    central index to create exports

---
+++ source/index.js
@@ -0,0 +1,4 @@
+module.exports = {
+    ...require("./ymdToDate"),
+    ...require("./generateMonthListFromPeriod")
+};
--- source/ymdToDate.js
+++ source/ymdToDate.js
@@ -1,5 +1,3 @@
-export {generateMonthListFromPeriod, generateMonthListBetweenTwoDates} from "./generateMonthListFromPeriod";
-
 /**
  * Convert a string to a date object. Expecting 2017-07-11.
  * @param dateStr
