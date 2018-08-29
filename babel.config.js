1b05236c1bd0d323991c52344364629a4fc73167
Kai Kasugai <kk@formitas.de>
2018-08-29 09:51:25 +0200

    date public module updated to babel 7 refs #2493

---
+++ babel.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  "presets" : ["@babel/preset-env"],
+  "plugins": [
+    "@babel/plugin-proposal-object-rest-spread",
+    "@babel/plugin-syntax-class-properties",
+    "@babel/plugin-proposal-class-properties",
+    "@babel/plugin-transform-modules-commonjs"
+  ]
+};
